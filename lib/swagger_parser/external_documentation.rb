8dafbdc87f117d130421b3d2d9cea8db98eb1507
Christopher Young <krsyoung@gmail.com>
2016-06-15 16:29:42 -0400

    add url and description to external documentation

--- lib/swagger_parser/external_documentation.rb
+++ lib/swagger_parser/external_documentation.rb
@@ -2,5 +2,14 @@ require "swagger_parser/source_based_object"
 
 module SwaggerParser
   class ExternalDocumentation < SourceBasedObject
+    # @return [Object]
+    def url
+      source["url"]
+    end
+
+    # @return [Object]
+    def description
+      source["description"]
+    end
   end
 end
